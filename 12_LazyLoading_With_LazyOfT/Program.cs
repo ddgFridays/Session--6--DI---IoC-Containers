c58567b6ccaa4d46e946f5ab0daa9d15fea4d32e
Alexander Webber <alexander.webber@dotdigitalgroup.co.uk>
2011-10-30 12:15:46 +0000

    Adding lazy loading and constructor injection examples

---
+++ 12_LazyLoading_With_LazyOfT/Program.cs
@@ -0,0 +1,54 @@
+﻿using System;
+using StructureMap;
+
+namespace _12_LazyLoading_With_LazyOfT
+{
+    internal class Program
+    {
+        private static void Main()
+        {
+            ObjectFactory.Configure(x =>
+            {
+                x.For(typeof(Lazy<>)).Use(typeof(Lazy<>)).CtorDependency<bool>("isThreadSafe").Is(true);
+                x.For<ExpensiveResource>();
+                x.For<ThingThatNeedsExpensiveResource>();
+            });
+            ObjectFactory.AssertConfigurationIsValid();
+
+            var thingThatNeedsExpensiveResource = ObjectFactory.GetInstance<ThingThatNeedsExpensiveResource>();
+            Console.WriteLine("Expensive resource has not been initialised yet");
+            thingThatNeedsExpensiveResource.UseExpensiveResource();
+
+            Console.ReadKey();
+        }
+    }
+
+    public class ExpensiveResource
+    {
+        public ExpensiveResource()
+        {
+            Console.WriteLine("Expensive resource has now been initialised!");
+        }
+
+        public void SayHello()
+        {
+            Console.WriteLine("Hello World");
+        }
+    }
+
+    public class ThingThatNeedsExpensiveResource
+    {
+        private readonly Lazy<ExpensiveResource> _expensiveResource;
+
+        public ThingThatNeedsExpensiveResource(Lazy<ExpensiveResource> expensiveResource)
+        {
+            _expensiveResource = expensiveResource;
+        }
+
+        public void UseExpensiveResource()
+        {
+            _expensiveResource.Value.SayHello();
+        }
+    }
+
+}
---
+++ 13_More_Constructur_Injection/Program.cs
@@ -0,0 +1,69 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using StructureMap;
+
+namespace _13_More_Constructur_Injection
+{
+    class Program
+    {
+        
+        static void Main(string[] args)
+        {
+            //If all the constructor arguments have differing types you can just do:
+            ObjectFactory.Initialize(x => x
+                .For<Person>()
+                .Use<Person>()
+                .Ctor<string>().Is("Alex")
+                .Ctor<int>().Is(15));
+
+            Console.WriteLine(ObjectFactory.GetInstance<Person>());
+
+            //otherwise you have to specify the names of the arguments like so:
+            ObjectFactory.Configure(x => x
+                .For<Point>()
+                .Use<Point>()
+                .Ctor<int>("x").Is(5)
+                .Ctor<int>("y").Is(2));
+
+            Console.WriteLine(ObjectFactory.GetInstance<Point>());
+
+            Console.ReadKey();
+        }
+    }
+
+    public class Person
+    {
+        private readonly string _name;
+        private readonly int _age;
+
+        public Person(string name, int age)
+        {
+            _name = name;
+            _age = age;
+        }
+
+        public override string ToString()
+        {
+            return string.Format("My name is {0} and I am {1} years old.", _name, _age);
+        }
+    }
+
+    public class Point
+    {
+        private readonly int _x;
+        private readonly int _y;
+
+        public Point(int x, int y)
+        {
+            _x = x;
+            _y = y;
+        }
+
+        public override string ToString()
+        {
+            return string.Format("X: {0}, Y: {1}", _x, _y);
+        }
+    }
+}
